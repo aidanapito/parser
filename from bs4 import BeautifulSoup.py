5f7fd98339548e5ee4f35b8380e5a0eb1b27d6ae
aidanapito <aidan2apito@gmail.com>
2023-11-06 15:16:46 -0500

    Update from bs4 import BeautifulSoup.py

--- from bs4 import BeautifulSoup.py
+++ from bs4 import BeautifulSoup.py
@@ -6,9 +6,30 @@ source = requests.get('https://rutgersnewarkathletics.com/sports/mens-volleyball
 
 soup = BeautifulSoup(source.text, 'html.parser')
 
-soup.find_all("tr", class_ ="odd")
-soup.find_all("tr", class_ ="even")
+odd_rows = soup.find_all("tr", class_="odd")
+even_rows = soup.find_all("tr", class_="even")
 
-Players= int
-df = pd.DataFrame(Players, columns = ['Position','Matches Played','Sets Played','Kills','Kills per Set','Attack Errors','Total Attacking Attempts', 'Hitting Percentage', 'Assists','Assists per Set','Service Aces', 'Service Aces per Set', 'Service Errors'])
-print (df)
+
+dataRutgersNewark = []
+
+for row in odd_rows + even_rows:
+    columns = row.find_all("td")
+    if len(columns) == 13:
+        player_data = {
+            'Position': columns[0].text.strip(),
+            'Matches Played': int(columns[1].text.strip()),
+            'Sets Played': int(columns[2].text.strip()),
+            'Kills': int(columns[3].text.strip()),
+            'Kills per Set': float(columns[4].text.strip()),
+            'Attack Errors': int(columns[5].text.strip()),
+            'Total Attacking Attempts': int(columns[6].text.strip()),
+            'Hitting Percentage': float(columns[7].text.strip()),
+            'Assists': int(columns[8].text.strip()),
+            'Assists per Set': float(columns[9].text.strip()),
+            'Service Aces': int(columns[10].text.strip()),
+            'Service Aces per Set': float(columns[11].text.strip()),
+            'Service Errors': int(columns[12].text.strip()),
+        }
+        dataRutgersNewark.append(player_data)
+df = pd.DataFrame(dataRutgersNewark)
+df
